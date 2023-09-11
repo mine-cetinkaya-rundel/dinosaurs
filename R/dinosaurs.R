17efc0168d08c4fc012c2b498b3130290e2eefe4
Mine Çetinkaya-Rundel <cetinkaya.mine@gmail.com>
2023-09-11 11:17:32 -0400

    Update with latest data

--- R/dinosaurs.R
+++ R/dinosaurs.R
@@ -10,6 +10,12 @@
 #'   \item{ecosystem}{Ecosystem the species lives in.}
 #'   \item{vore}{Vore of species: `carnivore`, `herbivore`, or `pescavore`.}
 #'   \item{name_mening}{Meaning of species name.}
+#'   \item{hybrid}{Whether the dinosaur is a hybrid (`TRUE`) or not (`FALSE`).}
+#'   \item{time_period}{When the dinosaur lived.}
+#'   \item{weight}{Weight of dinosaur (TO DO: Add units).}
+#'   \item{height}{Height of dinosaur (TO DO: Add units).}
+#'   \item{bite_force}{Bite force of dinosaur, in newtons.}
+#'   \item{speed}{Speed of dinosaur (TO DO: Add units).}
 #'   }
 #' @source Various, mostly Wikipedia.
 #' @keywords datasets
@@ -17,25 +23,35 @@
 #'
 #' library(ggplot2)
 #'
-#' # What dinosaurs eat
+#' # What dinosaurs ate
 #' ggplot(dinosaurs, aes(x = vore, fill = vore)) +
 #' geom_bar(show.legend = FALSE) +
 #'   labs(
-#'     title = "What dinosaurs eat 🦖",
+#'     title = "What dinosaurs ate 🦖",
 #'     x = "Vore",
 #'     y = "Count"
 #'   ) +
 #'   scale_fill_manual(values = c("firebrick1", "forestgreen")) +
 #'   theme_minimal()
 #'
-#' # Where dinosaurs live
+#' # Where dinosaurs lived
 #' ggplot(dinosaurs, aes(y = ecosystem, fill = ecosystem)) +
 #' geom_bar(show.legend = FALSE) +
 #'   labs(
-#'     title = "Where dinosaurs live 🌎",
+#'     title = "Where dinosaurs lived 🌎",
 #'     y = "Ecosystem",
 #'     x = "Count"
 #'   ) +
 #'   scale_fill_manual(values = c("chocolate3", "darkseagreen2", "deepskyblue3")) +
+#'   theme_minimal()
+#'
+#' # When dinosaurs lived
+#' ggplot(dinosaurs, aes(y = ecosystem, fill = ecosystem)) +
+#' geom_bar(show.legend = FALSE) +
+#'   labs(
+#'     title = "When dinosaurs lived 🗓️",
+#'     y = "Period",
+#'     x = "Count"
+#'   ) +
 #'   theme_minimal()
 "dinosaurs"
